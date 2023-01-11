2360b9cf17681bb3183e79a6af3277edfcb826b6
EC2 Default User <ec2-user@ip-172-31-24-99.ec2.internal>
2023-01-11 12:20:47 +0000

    認証と認可の実装まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 
 class AuthController extends Controller
 {
@@ -27,8 +30,24 @@ class AuthController extends Controller
 
         // データの取得
         $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
 
         //
-        var_dump($datum); exit;
+        $request->session()->regenerate();
+        return redirect()->intended('/task/list');
+    }
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('front.index');
+        }
     }
 }
