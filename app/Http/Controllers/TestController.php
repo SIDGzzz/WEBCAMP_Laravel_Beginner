b26ea8222af9e6cb8b371044e58232dd74cb21f4
EC2 Default User <ec2-user@ip-172-31-24-99.ec2.internal>
2023-01-15 20:51:51 +0000

    タスクの編集を書いた

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\TestPostRequest;
+use Illuminate\Support\Facades\DB;
 
 
 class TestController extends Controller
